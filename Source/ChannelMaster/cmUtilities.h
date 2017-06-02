3f9b8ebc55d42ad53d6b90ca17565ae0970c72f1
w5wc <w5wc@arrl.net>
2017-06-01 20:50:24 -0500

    v2.4.3

--- Source/ChannelMaster/cmUtilities.h
+++
@@ -1,4 +0,0 @@
-
-extern __declspec (dllexport) void print_cmbuff_parameters(const char* filename, int id);
-
-extern __declspec (dllexport) void WriteAudio(double seconds, int rate, int size, double* indata, int mode);
