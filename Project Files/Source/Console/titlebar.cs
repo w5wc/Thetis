18a2aa2dde7a71445f4fe6ff53d49a9903809bad
My Name <my-name@chromium.org>
2019-12-26 22:02:24 -0600

    v2.6.9-b6

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -35,7 +35,7 @@ namespace Thetis
     class TitleBar
     {
         public const string BUILD_NAME = "";
-        public const string BUILD_DATE = "(12/24/19)";
+        public const string BUILD_DATE = "(12/26/19)";
 
         public static string GetString()
         {
